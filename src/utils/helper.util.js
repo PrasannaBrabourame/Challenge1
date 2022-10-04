e34508cb5d6bf0960918b81cdca0932b9b2354a4
PrasannaBrabourame <prasanna18101994@gmail.com>
2022-10-04 15:39:11 +0800

    Added Regex Splitter

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -7,3 +7,34 @@
  * Last updated :  04 Oct 2022                                                  *
  ********************************************************************************/
 
+/**
+ * function used to split the students email address from the notification string
+ * @async
+ * @function splitStudentEmail
+ * @type {POST Method}
+ * @param {String} text - raw string with notification and students email address with @ symbol
+ * @returns {Array} List of students email addresses
+ */
+
+ 
+const splitStudentEmail = (text) => {
+     const expressions = {
+         main: /@(.*?com)/gm,
+     }
+     try {
+         let result = expressions.main.exec(text)
+         if (result) {
+             result = result.map(s => s.trim())
+         }
+         return {
+         }
+     } catch (e) {
+         return {
+         }
+     }
+ }
+ 
+
+module.exports = {
+    splitStudentEmail
+}
