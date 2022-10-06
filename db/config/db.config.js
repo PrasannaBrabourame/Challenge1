42824cc153da8fc4fd2568a3449d7536fb6d0172
PrasannaBrabourame <prasanna18101994@gmail.com>
2022-10-06 17:26:29 +0800

    Added Migrations

---
+++ db/config/db.config.js
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ *                                                                              *
+ * Author       :  Prasanna Brabourame                                          *
+ * Version      :  1.0.0                                                        *
+ * Date         :  04 Sep 2022                                                  *
+ * Author       :  https://github.com/PrasannaBrabourame                        *
+ * Last updated :  04 Oct 2022                                                  *
+ ********************************************************************************/
+require('dotenv').config()
+
+const db = {
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+      }
+}
+module.exports = db;
