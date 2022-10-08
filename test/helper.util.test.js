99aafdad5f7c97b05d629587992fd6f68f55f4b7
PrasannaBrabourame <prasanna18101994@gmail.com>
2022-10-08 21:43:42 +0800

    Added API testing Super Test

---
+++ test/helper.util.test.js
@@ -0,0 +1,75 @@
+/*********************************************************************************
+ *                                                                              *
+ * Author       :  Prasanna Brabourame                                          *
+ * Version      :  1.0.0                                                        *
+ * Date         :  04 Sep 2022                                                  *
+ * Author       :  https://github.com/PrasannaBrabourame                        *
+ * Last updated :  08 Oct 2022                                                  *
+ ********************************************************************************/
+ const {
+	splitStudentEmail,
+	arrayToObject,
+	objectArrayFormator
+} = require('../src/utils/helper.util')
+describe('Helper Util : Student Email Split true response', () => {
+	test('String Process to split names with @', () => {
+		expect(splitStudentEmail('Hello students! @studentagnes@gmail.com @studentmiche@gmail.com')).toEqual({
+			status: true,
+			result: expect.any(Array)
+		});
+	});
+	test('String Process to split names with @ false response', () => {
+		expect(splitStudentEmail('Hello students!')).toEqual({
+			status: false,
+			error: 'No students email address found'
+		});
+	});
+	test('String Process to split names with @ false response', () => {
+		expect(splitStudentEmail()).toEqual({
+			status: false,
+			error: 'Improper Input Details'
+		});
+	});
+	test('String Process to split names with @ false response', () => {
+		expect(splitStudentEmail(123)).toEqual({
+			status: false,
+			error: 'Improper Input Details'
+		});
+	});
+	test('String Process to split names with @', () => {
+		expect(splitStudentEmail('Hello students! @studentagnes@gmail.com @studentmiche@gmail.com @testingstudent')).toEqual({
+			status: true,
+			result: expect.any(Array)
+		});
+	});
+});
+describe('Helper Util : Array to Object', () => {
+	test('Array to Object - Case 1', () => {
+		expect(arrayToObject([1], 'test')).toEqual([{
+			"test": 1
+		}]);
+	});
+	test('Array to Object - Case 2', () => {
+		expect(arrayToObject(['Hello'], 'Hello')).toEqual([{
+			"Hello": 'Hello'
+		}]);
+	});
+});
+describe('Helper Util : Object Array Formattor', () => {
+	test('Object Array Formattor- Case 1', () => {
+		expect(objectArrayFormator([{
+			"test": 1
+		}], 'test')).toEqual([
+			1
+		]);
+	});
+	test('Object Array Formattor - Case 2', () => {
+		expect(objectArrayFormator([{
+			"test": 1
+		}, {
+			"test": "test"
+		}], 'test')).toEqual([
+			1, "test"
+		]);
+	});
+});
