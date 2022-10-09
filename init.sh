76599ea08070e5de37d325eabdf2193b7d2917b8
PrasannaBrabourame <prasanna18101994@gmail.com>
2022-10-09 10:26:46 +0800

    Added Readme

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ echo server started at pid $pid
 npm run rollback
 npm run migrate
 npm run test
+npm run rollback
 kill $pid
 echo server stopped.
 
--- server.js
+++ server.js
@@ -29,7 +29,7 @@
  app.disable('x-powered-by') // Removing x-powered-by header
  app.use(compression()) // Gzip compression
  app.get('/', (req, res) => {
-     res.json({
+     res.status(200).json({
          'message': 'ok'
      });
  })
@@ -37,12 +37,12 @@
  app.use('/api', notificationRouter);
  /* Error handler middleware for Status 404 */
  app.use((req, res, next) => {
-         res.status(404).send("Sorry can't find that!")
+         res.status(404).send({"message":"Sorry can't find that!"})
  })
-     /* Error handler middleware for Status 500 */
+ /* Error handler middleware for Status 500 */
  app.use((err, req, res, next) => {
      console.error(err.stack)
-     res.status(500).send('Something broke!')
+     res.status(500).send({'message':'Something broke!'})
  })
  app.listen(port, '0.0.0.0', () => {
      console.log(`Example app listening at http://localhost:${port}`)
